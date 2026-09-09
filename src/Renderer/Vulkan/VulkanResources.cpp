f5d610d225abdd44505fa8d3ac391d35e4dafa77
Mohamed Tarek <149931382+WhiteWhale52@users.noreply.github.com>
2026-09-09 10:15:19 -0400

    Finished adding the image to the VulkanResources.* files and it compiles with no errors

--- src/Renderer/Vulkan/VulkanResources.cpp
+++ src/Renderer/Vulkan/VulkanResources.cpp
@@ -12,7 +12,7 @@ namespace Renderer::Vulkan {
 		bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
 
 
-		VmaAllocationCreateInfo allocInfo;
+		VmaAllocationCreateInfo allocInfo{};
 		allocInfo.usage = VMA_MEMORY_USAGE_CPU_TO_GPU;
 		allocInfo.flags = VMA_ALLOCATION_CREATE_MAPPED_BIT;
 
@@ -35,6 +35,84 @@ namespace Renderer::Vulkan {
 	void Buffer::Destroy(const Core::Vulkan::VulkanContext& context) {
 		if (context.vmaAllocator && handle) {
 			vmaDestroyBuffer(context.vmaAllocator, static_cast<VkBuffer>(handle), allocation);
+		}
+	}
+
+
+	void Image::Create2D(const Core::Vulkan::VulkanContext& context, uint32_t width, uint32_t height, VkFormat format, VkImageUsageFlags usageFlags, VkImageAspectFlags aspectFlags, VmaMemoryUsage memoryUsage)
+	{
+		this->height = height;
+		this->width = width;
+		this->aspectFlags = aspectFlags;
+		this->format = format;
+
+		VkImageCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+		createInfo.imageType = VK_IMAGE_TYPE_2D;
+		createInfo.extent = { width, height, 1 };
+		createInfo.mipLevels = 1;
+		createInfo.arrayLayers = 1;
+		createInfo.format = format;
+		createInfo.tiling = VK_IMAGE_TILING_OPTIMAL;
+		createInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+		createInfo.usage = usageFlags;
+		createInfo.samples = VK_SAMPLE_COUNT_1_BIT;
+		createInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+		VmaAllocationCreateInfo allocationCreateInfo{};
+		allocationCreateInfo.usage = memoryUsage;
+
+		VkImage rawImage = VK_NULL_HANDLE;
+		if (vmaCreateImage(context.vmaAllocator, &createInfo, &allocationCreateInfo, &rawImage, &allocation, nullptr) != VK_SUCCESS) {
+			throw std::runtime_error("Failed to create image using VMA in VulkanResources.cpp");
+		}
+		handle = vk::Image(rawImage);
+
+		vk::ImageViewCreateInfo imageViewCreateInfo;
+		imageViewCreateInfo.image = rawImage;
+		imageViewCreateInfo.viewType = vk::ImageViewType::e2D;
+		imageViewCreateInfo.format = vk::Format(format);
+		imageViewCreateInfo.subresourceRange.aspectMask = vk::ImageAspectFlags(aspectFlags);
+		imageViewCreateInfo.subresourceRange.baseMipLevel = 0;
+		imageViewCreateInfo.subresourceRange.levelCount = 1;
+		imageViewCreateInfo.subresourceRange.baseArrayLayer = 0;
+		imageViewCreateInfo.subresourceRange.layerCount = 1;
+
+		vk::ImageView rawView = VK_NULL_HANDLE;
+		if (context.logicalDevice.createImageView(&imageViewCreateInfo, nullptr, &rawView) != vk::Result::eSuccess) {
+			throw std::runtime_error("Failed to create an image view in VulkanResources.cpp");
+		}
+
+		view = vk::ImageView(rawView);
+	}
+
+	void Image::TransitionLayout(vk::CommandBuffer commandBuffer, vk::ImageLayout oldLayout, vk::ImageLayout newLayout, vk::PipelineStageFlags srcStage, vk::PipelineStageFlags dstStage, vk::AccessFlags srcAccess, vk::AccessFlags dstAccess)
+	{
+		vk::ImageMemoryBarrier memoryBarrier;
+		memoryBarrier.oldLayout = oldLayout;
+		memoryBarrier.newLayout = newLayout;
+		memoryBarrier.srcQueueFamilyIndex = vk::QueueFamilyIgnored;
+		memoryBarrier.dstQueueFamilyIndex = vk::QueueFamilyIgnored;
+		memoryBarrier.image = handle;
+		memoryBarrier.subresourceRange.aspectMask = vk::ImageAspectFlags(aspectFlags);
+		memoryBarrier.subresourceRange.baseMipLevel = 0;
+		memoryBarrier.subresourceRange.baseArrayLayer = 0;
+		memoryBarrier.subresourceRange.levelCount = 1;
+		memoryBarrier.subresourceRange.layerCount = 1;
+		memoryBarrier.srcAccessMask = srcAccess;
+		memoryBarrier.dstAccessMask = dstAccess;
+
+		commandBuffer.pipelineBarrier(srcStage, dstStage, {}, 0, nullptr, 0, nullptr, 1, &memoryBarrier);
+
+	}
+
+	void Image::Destroy(const Core::Vulkan::VulkanContext& context)
+	{
+		if (context.logicalDevice && view) {
+			context.logicalDevice.destroyImageView(view);
+		}
+		if (context.vmaAllocator && handle) {
+			vmaDestroyImage(context.vmaAllocator, static_cast<VkImage>(handle), allocation);
 		}
 	}
 }
--- src/Renderer/Vulkan/VulkanResources.h
+++ src/Renderer/Vulkan/VulkanResources.h
@@ -1,4 +1,4 @@
-#pragma once
+	#pragma once
 #include <VulkanCore/Config/CommonHeaders.h>
 #include <VulkanCore/Config/ResourcesConfigs.h>
 #include <VulkanCore/VulkanContext.h>
@@ -13,6 +13,25 @@ namespace Renderer::Vulkan {
 
 		void Create(const Core::Vulkan::VulkanContext& context);
 		void Destroy(const Core::Vulkan::VulkanContext& context);
+	};
+
+	struct Image {
+		void Create2D(const Core::Vulkan::VulkanContext& context, uint32_t width, uint32_t height, VkFormat format, VkImageUsageFlags usageFlags, VkImageAspectFlags aspectFlags, VmaMemoryUsage memoryUsage = VMA_MEMORY_USAGE_GPU_ONLY);
+
+		void TransitionLayout(vk::CommandBuffer commandBuffer, vk::ImageLayout oldLayout, vk::ImageLayout newLayout, vk::PipelineStageFlags srcStage, vk::PipelineStageFlags dstStage, vk::AccessFlags srcAccess, vk::AccessFlags dstAccess);
+
+		void Destroy(const Core::Vulkan::VulkanContext& context);
+
+		vk::Image handle = VK_NULL_HANDLE;
+		vk::ImageView view = VK_NULL_HANDLE;
+		VmaAllocation allocation = VK_NULL_HANDLE;
+		
+		uint32_t width = 0;
+		uint32_t height = 0;
+
+		VkFormat format = VK_FORMAT_UNDEFINED;
+		VkImageAspectFlags aspectFlags = VK_IMAGE_ASPECT_COLOR_BIT;
+
 	};
 
 
